79cffd46fc8dcee2805dbe6cb41262f338e06e57
Night-Cheza <leila.kassi@outlook.com>
2022-03-06 14:50:33 -0700

    Initial setup of the project

---
+++ src/main/java/ca/sait/jdbclab6/models/Role.java
@@ -0,0 +1,36 @@
+package ca.sait.jdbclab6.models;
+
+import java.io.Serializable;
+
+/**
+ * Represents a role
+ * @author Leila Nalivkina, Nick Hemnett
+ */
+public class Role implements Serializable {
+	private int id;
+	private String roleName;
+
+	public Role() {
+	}
+
+	public Role(int id, String roleName) {
+		this.id = id;
+		this.roleName = roleName;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getRoleName() {
+		return roleName;
+	}
+
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+}
